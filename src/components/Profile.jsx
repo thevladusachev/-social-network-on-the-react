a2e05d6eb7fd2aa980c44ce7fa20bd9a08464c92
Vlad Usachev <Vladuschv@yandex.ru>
2023-01-18 13:44:39 +0300

    css-modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
 	return (
-		<div className='content'>
+		<div className={s.content}>
 			<div>
-				<img className='content__img' src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' alt='mountain'></img>
+				<img className={s.content__img} src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' alt='mountain'></img>
 			</div>
 			<div>ava</div>
 			<div>posts
