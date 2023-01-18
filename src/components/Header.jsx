61c36774e7de9010a21b2268fdd77696bf3db2dd
Vlad Usachev <Vladuschv@yandex.ru>
2023-01-18 12:43:11 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<img src='https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png' alt="logo"></img>
+		</header>
+	);
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img className='content__img' src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' alt='mountain'></img>
+			</div>
+			<div>ava</div>
+			<div>posts
+				<div>new post</div>
+				<div>post 1</div>
+				<div>post 2</div>
+			</div>
+		</div>
+	);
+}
+
+export default Profile;
