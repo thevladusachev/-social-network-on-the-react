dbe6d2f9fb366de0d8e144f94e6f4051dd39abdd
Vlad Usachev <Vladuschv@yandex.ru>
2023-01-22 12:54:07 +0300

    adding dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,31 @@ import s from './Dialogs.module.css';
 const Dialogs = (props) => {
 	return (
 		<div className={s.dialogs}>
-			Dialogs
+			<div className={s.dialogsItems}>
+				<div className={s.dialog}>
+					Andrey
+				</div>
+				<div className={s.dialog}>
+					Dima
+				</div>
+				<div className={s.dialog}>
+					Kirill
+				</div>
+				<div className={s.dialog}>
+					Misha
+				</div>
+				<div className={s.dialog}>
+					Lida
+				</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>hi!</div>
+				<div className={s.message}>How are you?</div>
+				<div className={s.message}>YESS!!!</div>
+				<div className={s.message}>no, but...</div>
+				<div className={s.message}>I love u!</div>
+
+			</div>
 		</div>
 	);
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,22 +5,22 @@ import s from './Navbar.module.css';
 const Nav = () => {
 	return (
 		<nav className={s.nav}>
-			<div>
-				<NavLink to={'/profile'}>Profile</NavLink >
+			<div className={s.item}>
+				<NavLink to={'/profile'} className={navData => navData.isActive ? s.active : s.item}>Profile</NavLink >
 			</div>
 			<div>
-				<NavLink to={'/dialogs'}>Message</NavLink>
+				<NavLink to={'/dialogs'} className={navData => navData.isActive ? s.active : s.item}>Message</NavLink>
 			</div>
 			<div>
-				<NavLink to={'/news'}>News</NavLink>
+				<NavLink to={'/news'} className={navData => navData.isActive ? s.active : s.item}>News</NavLink>
 			</div>
 			<div>
-				<NavLink to={'/music'}>Music</NavLink>
+				<NavLink to={'/music'} className={navData => navData.isActive ? s.active : s.item}>Music</NavLink>
 			</div>
 			<div>
-				<NavLink to={'/settings'}>Settings</NavLink>
+				<NavLink to={'/settings'} className={navData => navData.isActive ? s.active : s.item}>Settings</NavLink>
 			</div>
-		</nav>
+		</nav >
 	);
 }
 
